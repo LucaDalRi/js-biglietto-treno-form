0e3017292a69c12342fed00ee782b664faae2cdd
LucaDalRi <euro2xd@gmail.com>
2022-12-20 19:52:48 +0100

    Costo Biglietto NaN ???????

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,48 @@
 const nomeCognome = document.getElementById('nomeCognome');
 const kmDaPercorrere = document.getElementById('kmDaPercorrere');
 const age = document.getElementById('age');
+const buttonSubmit = document.getElementById('buttonSubmit');
+const prezzoPerKm = 0.21;
+let prezzoViaggio = (prezzoPerKm * kmDaPercorrere);
+const scontoQuaranta = ((prezzoViaggio * 40) / 100);
+const scontoVenti = ((prezzoViaggio * 20) / 100);
 
+buttonSubmit.addEventListener('click',
+
+    function() {
+
+        // console.log('cliccato su submit');
+
+        // console.log('Il valore di kmDaPercorrere è', kmDaPercorrere.value)
+
+        // console.log('Il valore di nomeCognome è', nomeCognome.value);
+
+        // console.log('Il valore di age è', age.value);
+
+        if (age.value == '17') {
+
+            prezzoViaggio = (prezzoViaggio - scontoVenti);
+
+            prezzoViaggio = prezzoViaggio.toFixed(2);
+            
+            document.getElementById('prezzo').innerHTML = prezzoViaggio + ' €';
+            
+            // console.log(typeof prezzoViaggio);
+
+            document.getElementById('nome').innerHTML = nomeCognome.value;
+
+            document.getElementById('biglietto').innerHTML = 'Biglietto Young';
+
+            document.getElementById('nome').innerHTML = nomeCognome.value;
+
+
+        }
+        else if (age == '65') {
+
+
+
+        }
+
+    }
+
+);
