f5d3eafe684427acc9da1498778d00bcfe9d7f8f
LucaDalRi <euro2xd@gmail.com>
2022-12-20 17:03:43 +0100

    Inizio script

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+// const etaPasseggiero = prompt('Inserisci la tua età');
+// const kmDaPercorrere = prompt('Quanti kilomentri devi percorrere?');
+// const prezzoPerKm = 0.21;
+// let prezzoViaggio = (prezzoPerKm * kmDaPercorrere);
+
+// let scontoQuaranta = ((prezzoViaggio * 40) / 100)
+// let scontoVenti = ((prezzoViaggio * 20) / 100)
+
+// if (etaPasseggiero > 65) {
+//     prezzoViaggio = (prezzoViaggio - scontoQuaranta);
+//     prezzoViaggio = prezzoViaggio.toFixed(2);
+//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
+// }
+
+// else if (etaPasseggiero < 18) {
+//     prezzoViaggio = (prezzoViaggio - scontoVenti);
+//     prezzoViaggio = prezzoViaggio.toFixed(2);
+//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
+// }
+
+// else {
+//     prezzoViaggio = prezzoViaggio.toFixed(2);
+//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
+// }
+
+
+const nomeCognome = document.getElementById('nomeCognome');
+const kmDaPercorrere = document.getElementById('kmDaPercorrere');
+const age = document.getElementById('age');
+
