2471bc4b9cb49e4cb3e6c7f56ff9d6b8e5d654d1
LucaDalRi <euro2xd@gmail.com>
2022-12-22 16:28:45 +0100

    Corretto Il NaN e il .value Grazie Mauro

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,21 @@
-
-// const etaPasseggiero = prompt('Inserisci la tua età');
-// const kmDaPercorrere = prompt('Quanti kilomentri devi percorrere?');
-// const prezzoPerKm = 0.21;
-// let prezzoViaggio = (prezzoPerKm * kmDaPercorrere);
-
-// let scontoQuaranta = ((prezzoViaggio * 40) / 100)
-// let scontoVenti = ((prezzoViaggio * 20) / 100)
-
-// if (etaPasseggiero > 65) {
-//     prezzoViaggio = (prezzoViaggio - scontoQuaranta);
-//     prezzoViaggio = prezzoViaggio.toFixed(2);
-//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
-// }
-
-// else if (etaPasseggiero < 18) {
-//     prezzoViaggio = (prezzoViaggio - scontoVenti);
-//     prezzoViaggio = prezzoViaggio.toFixed(2);
-//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
-// }
-
-// else {
-//     prezzoViaggio = prezzoViaggio.toFixed(2);
-//     document.getElementById('prova').innerHTML = 'Il totale è di => ' + prezzoViaggio + ' €';
-// }
-
-
 const nomeCognome = document.getElementById('nomeCognome');
-const kmDaPercorrere = document.getElementById('kmDaPercorrere');
 const age = document.getElementById('age');
 const buttonSubmit = document.getElementById('buttonSubmit');
 const prezzoPerKm = 0.21;
-let prezzoViaggio = (prezzoPerKm * kmDaPercorrere);
-const scontoQuaranta = ((prezzoViaggio * 40) / 100);
-const scontoVenti = ((prezzoViaggio * 20) / 100);
 
+console.log(kmDaPercorrere.value);
 
 buttonSubmit.addEventListener('click',
 
     function() {
 
+        const kmDaPercorrere = document.getElementById('kmDaPercorrere').value;
+                                                                                    // da quando premo il tasto vado a prendere il VALORE DELL INPUT
+        let prezzoViaggio = (prezzoPerKm * kmDaPercorrere);                         // .value prende il valore interno di quello che vado a puntare con document.getElementById per esempio 
+
         if (age.value == 'Minorenne') {
+
+            const scontoVenti = ((prezzoViaggio * 20) / 100);
 
             prezzoViaggio = (prezzoViaggio - scontoVenti);
 
@@ -55,10 +31,11 @@ buttonSubmit.addEventListener('click',
 
             document.getElementById('codice').innerHTML = (Math.random(1000, 9999) * 10000).toFixed(0) // Non so neanche perchè funzioni
 
-
         }
         
         else if (age.value == 'Over') {
+
+            const scontoQuaranta = ((prezzoViaggio * 40) / 100);
 
             prezzoViaggio = (prezzoViaggio - scontoQuaranta);
 
